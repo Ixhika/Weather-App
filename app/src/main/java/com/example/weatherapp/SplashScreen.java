60aa0182175607590e60f9208eb94aa50a27e236
Ixhika <106822722+Ixhika@users.noreply.github.com>
2023-09-28 20:46:30 +0530

    splashscreen

---
+++ app/src/main/java/com/example/weatherapp/SplashScreen.java
@@ -0,0 +1,36 @@
+package com.example.weatherapp;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+import android.view.Window;
+import android.widget.TextView;
+
+import com.airbnb.lottie.LottieAnimationView;
+
+public class SplashScreen extends AppCompatActivity {
+    TextView name;
+    LottieAnimationView lottie;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        Window window = this.getWindow();
+        window.setStatusBarColor(this.getResources().getColor(R.color.darkBlue));
+        setContentView(R.layout.activity_splash_screen);
+        name = findViewById(R.id.appname);
+        lottie = findViewById(R.id.lottie);
+
+        name.animate().translationY(-900).setDuration(2700).setStartDelay(0);
+        name.animate().translationX(2000).setDuration(2000).setStartDelay(2900);
+        new Handler().postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                Intent intent = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(intent);
+            }
+        },3000);
+    }
+}
