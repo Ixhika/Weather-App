2557455926ec9e5e05d6ba817497f72061598287
Ixhika <106822722+Ixhika@users.noreply.github.com>
2023-10-04 12:58:03 +0530

    jsonfileuploaded

---
+++ app/src/main/java/com/example/weatherapp/Coord.java
@@ -0,0 +1,5 @@
+package com.example.weatherapp; 
+public class Coord{
+    public double lon;
+    public double lat;
+}
---
+++ app/src/main/java/com/example/weatherapp/Wind.java
@@ -0,0 +1,5 @@
+package com.example.weatherapp; 
+public class Wind{
+    public double speed;
+    public int deg;
+}
